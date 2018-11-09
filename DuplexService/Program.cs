ff8a8d9e0abb3ff3f7cd19da21b0918e91dc010f
Stefan Stipanovic <s_stefan@outlook.com>
2018-11-09 16:37:55 +0100

    Add project files.

---
+++ DuplexService/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DuplexService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
